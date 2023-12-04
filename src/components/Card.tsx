5a67c2b39a2b44fad04e5f9d1dcce0d322258fda
lecav30 <lecav30@outlook.com>
2023-12-04 00:17:18 -0500

    feat: :sparkles: add card info

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,22 +4,32 @@ interface Props {
   title: string;
   imageUrl: string;
   altImage: string;
+  description: string;
   skills: string[];
 }
 
 const Card: FC<Props> = (props) => {
+  const [showTechnologies, setShowTechnologies] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
 
   const handleShowAbout = () => {
     setShowAbout(!showAbout);
   };
 
+  const handleShowTechnologies = () => {
+    setShowTechnologies(!showTechnologies);
+  };
+
   return (
     <div
-      className="card p-4 rounded-xl shadow-lg md:w-3/12 cursor-pointer
-     hover:border-[#6600FF] hover:border-s-4 hover:border-b-[3px] min-h-[300px]"
-      onClick={handleShowAbout}
+      className="card p-4 rounded-xl shadow-lg md:w-3/12 hover:border-[#6600FF]
+      hover:border-s-4 hover:border-b-[3px] min-h-[300px] max-w-[310px] md:max-w-none relative"
     >
+      <button onClick={handleShowAbout} className="w-8 absolute top-4 right-4">
+        <img src="src/assets/svg/information.svg" alt="Information" />
+      </button>
+      <br />
+      <br />
       {!showAbout && (
         <div
           id="presentation"
@@ -34,13 +44,24 @@ const Card: FC<Props> = (props) => {
         </div>
       )}
       {showAbout && (
-        <div id="about">
+        <div id="about" className="p-4 flex flex-col">
           <h1 className="text-2xl">{props.title}</h1>
-          <ul id="skills">
-            {props.skills.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
+          <p className="mt-4">{props.description}</p>
+          <button
+            className="my-4 bg-[var(--main-color)] py-1 px-3 border border-black rounded-3xl text-white w-fit self-center"
+            onClick={handleShowTechnologies}
+          >
+            Show technologies
+          </button>
+          {showTechnologies && (
+            <ul id="skills" className="flex flex-wrap">
+              {props.skills.map((skill, index) => (
+                <li key={index} className="px-4">
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       )}
     </div>
